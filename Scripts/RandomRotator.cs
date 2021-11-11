cfbf36c42025243cb44c37d0bedb6213c47fe6ef
Sergei Haiduk <sergio13082000@gmail.com>
2021-11-11 04:06:51 +0200

    fixed some warning, edited comments

--- Scripts/RandomRotator.cs
+++ Scripts/RandomRotator.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Class RandomRotator, used to implement 
+ * rotation of asteroid objects around their axis in a random direction.
+ */
 public class RandomRotator : MonoBehaviour
 {
-    public float tumble;
-    private Rigidbody rigidbody;
+    public float tumble;  // rotation rate
+    private new Rigidbody rigidbody;
     
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        rigidbody.angularVelocity = new Vector3(1, 1, 1) * tumble; // задаем направление угловой скорости вращения объекта
+
+        // set direction of the angular velocity of rotation of the object
+        rigidbody.angularVelocity = new Vector3(1, 1, 1) * tumble; 
 
         rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
     }
